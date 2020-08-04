07f47f7e78acecc79381e914925dac5d77ce5cf1
Eduard Kerkhoven <eduardk@chalmers.se>
2020-08-04 11:45:01 +0200

    feat: add THF shunt in enzyme usage output

--- code/boxplotEnzymeUsage.R
+++ code/boxplotEnzymeUsage.R
@@ -18,18 +18,19 @@ GO <- read.delim('../../data/selectedAnnotation.txt', stringsAsFactors = F)
 capUse <- capUse[capUse$protID %in% GO$Entry,]
 idx <- match(capUse$protID, GO$Entry)
 capUse$GOterm <- gsub('TCA','TCA cycle',GO$system[idx])
+capUse$GOterm <- gsub('PPP','PP shunt',capUse$GOterm)
 colnames(capUse) <- gsub('capUse_','',colnames(capUse))
 
 capUse <- capUse %>% mutate_if(is.numeric, round, digits = 3)
 write_delim(capUse,'../../results/enzymeUsage/capUsage.txt',delim = '\t')
 
 capUse <- gather(capUse, 'Condition', 'Usage', 4:8)
-capUse$GOterm <- factor(capUse$GOterm, levels=c('Glycolysis','TCA cycle','ETC','Ribosome'))
+capUse$GOterm <- factor(capUse$GOterm, levels=c('Glycolysis','TCA cycle','ETC','PP shunt','Ribosome'))
 capUse$Condition <- factor(capUse$Condition, levels=c('CN4','CN22','CN38','CN78','hGR'))
 
 ggplot(capUse, aes(x = Condition, y = Usage, color=GOterm)) +
   geom_boxplot(lwd = 0.35) +
-  scale_color_manual(values=c('#CBBBA0','#1D1D1B','#1D71B8','#878787')) +
+  scale_color_manual(values=c('#CBBBA0','#1D1D1B','#1D71B8','#878787','#878787')) +
   facet_grid(. ~ GOterm) +
   labs(x = '', y = 'Capacity usage (%)') + 
   theme_classic() +
